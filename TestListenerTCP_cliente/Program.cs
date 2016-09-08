9bab7c6bda5a5c39040b13b4bf80cab119b9a5cb
lquenta <lquenta@gmail.com>
2016-09-08 11:36:52 -0400

    first commit

---
+++ TestListenerTCP_cliente/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestListenerTCP_cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+
+            try
+            {
+                // Create a TcpClient. 
+                // The client requires a TcpServer that is connected 
+                // to the same address specified by the server and port 
+                // combination
+                Console.WriteLine("puerto:");
+                int port = Int32.Parse(Console.ReadLine());
+                Console.Write("Escribir IP a conectarse:");
+                string ip = Console.ReadLine(); ;
+                TcpClient client = new TcpClient(ip, port);
+                Console.WriteLine("escribir mensaje:");
+                string message = Console.ReadLine();
+
+                
+                byte[] bytes = new byte[256];
+                // Translate the passed message into ASCII and store it as a byte array.
+                Byte[] data = new Byte[256];
+                data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing. 
+                // Stream stream = client.GetStream();
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                output = "Sent: " + message;
+                Console.WriteLine(output);
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes23 = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes23);
+                output = "Received: " + responseData;
+                Console.WriteLine(output);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException e)
+            {
+                output = "SocketException: " + e.ToString();
+                Console.WriteLine(output);
+            }
+            catch (Exception ex)
+            {
+                output = "SocketException: " + ex.ToString();
+                Console.WriteLine(output);
+            }
+
+            Console.ReadLine();
+            /*
+             try
+            {
+                //obtain DNS host name
+                //string ip = GetPublicIP();
+                Console.Write("Escribir IP a conectarse:");
+                string ip = Console.ReadLine(); ;
+                IPAddress IP_address = IPAddress.Parse(ip);
+                System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient();
+                client.Connect(IP_address, 3307);
+                try
+                {
+                    while (true)
+                    {
+
+                        Stream s = client.GetStream();
+                        StreamReader sr = new StreamReader(s);
+                        StreamWriter sw = new StreamWriter(s);
+                        sw.AutoFlush = true;
+                        Console.WriteLine("escriba mensaje");
+                        string tramaEnvuiar = " M`☻◄☺G☺        ☻ `4128000108231624000115123456781234FFFF00000005550 0174128";
+                        sw.WriteLine(tramaEnvuiar);
+                        while (true)
+                        {
+                            string respuesta = sr.ReadLine();
+                            Console.WriteLine(respuesta);
+                            break;
+                        }
+
+                    }
+                }
+                finally
+                {
+                    // code in finally block is guranteed 
+                    // to execute irrespective of 
+                    // whether any exception occurs or does 
+                    // not occur in the try block
+                    client.Close();
+                }
+            }catch(Exception ex){
+                Console.WriteLine(ex.Message);
+            }*/
+        }
+    }
+}
---
+++ testConsola/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace testConsola
+{
+    class Program
+    {
+        static TcpListener listener;
+        const int LIMIT = 1;
+        static string localhost_ip;
+        static string Get_ip_local_address()
+        {
+            System.Net.IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                Console.WriteLine("ip encontrado:" + ip.ToString());
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                }
+               
+
+            }
+            Console.WriteLine("ip servidor");
+            localIP = Console.ReadLine();
+            return localIP;
+        }
+        static string output = "";
+        static void Main(string[] args)
+        {
+            TcpListener tcpListener = null;
+            localhost_ip = Get_ip_local_address();
+            IPAddress ipAddress = IPAddress.Parse(localhost_ip);
+            //IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            try
+            {
+                Console.WriteLine("Puerto:");
+                int port = Int32.Parse(Console.ReadLine());
+                tcpListener = new TcpListener(ipAddress, port);
+                tcpListener.Start();
+                output = ipAddress + "Esperando conexion en ..."  ;
+                Console.Write(output);
+            }
+            catch (Exception ex)
+            {
+                output = "Error:" + ex.ToString();
+                Console.WriteLine(output);
+            }
+            while (true)
+            {
+                Thread.Sleep(10);
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                byte[] bytes = new byte[256];
+                NetworkStream stream = tcpClient.GetStream();
+                Byte[] data = new Byte[256];
+                Int32 bytes23 = stream.Read(data, 0, data.Length);
+                string responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes23);
+                output = "Received: " + responseData;
+                Console.WriteLine(output);
+                Console.WriteLine("Escribir respuesta");
+                string mandarRespuesta = Console.ReadLine();
+
+
+                SocketHelper helper = new SocketHelper();
+                Console.WriteLine("escritura en stream");
+                helper.processMsg(tcpClient, stream, mandarRespuesta);
+                stream.Flush();
+                Console.ReadLine();
+                
+
+
+            }
+
+        }
+
+    }
+    class SocketHelper
+    {
+        TcpClient msClient;
+        string mstrMessage;
+        string mstrResponse;
+        byte[] bytesSent;
+        public void processMsg(TcpClient client, NetworkStream stream,string respuesta)
+        {
+                //mstrResponse = "test de trama desde gateway";
+
+
+                //armar la trama de respuesta
+
+                //mstrResponse = "Repsuesta";
+
+            bytesSent = Encoding.ASCII.GetBytes(respuesta);
+            stream.Write(bytesSent, 0, bytesSent.Length);
+            stream.Flush();
+            Console.WriteLine("Respuesta enviada");
+        }
+    }
+}
